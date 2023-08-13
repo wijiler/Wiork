fbe53c78e89992f10501dcc31f610c6c2ad6c0c6
wij <41879253+wijiler@users.noreply.github.com>
2023-08-12 21:38:11 -0400

    Projection camera

--- src/main/kotlin/Rendering/Cameras/Projection.kt
+++ src/main/kotlin/Rendering/Cameras/Projection.kt
@@ -1,14 +1,19 @@
 package Rendering.Cameras
 
 import Rendering.Vectors
+import org.joml.Matrix4f
 import org.joml.Vector3f
 // Orthographic camera -> https://learnopengl.com/Getting-started/Camera
-class Ortho {
+class Projection {
     var pos : Vector3f = Vector3f(0f,0f,0f)
     private var cameraTarget = Vector3f(0f,0f,0f)
     var cameraDir = Vectors.normalize((Vectors.subtract(pos,cameraTarget)))
     private var up = Vector3f(0f,1f,0f)
     var camRight = Vectors.normalize(Vectors.cross_product(up,cameraDir))
     var camUp = Vectors.cross_product(cameraDir,camRight)
-
+    var view: Matrix4f = Vectors.lookAt(
+        Vector3f(0f,0f,3f),
+        Vector3f(0f,0f,0f),
+        Vector3f(0f,1f,0f)
+    )
 }
--- src/main/kotlin/Rendering/Vectors.kt
+++ src/main/kotlin/Rendering/Vectors.kt
@@ -1,10 +1,12 @@
 package Rendering
 
+import org.joml.Math.cos
 import org.joml.Math.sqrt
 import org.joml.Matrix4f
 import org.joml.Vector3f
 import org.joml.Vector4f
 import org.lwjgl.BufferUtils
+import java.nio.FloatBuffer
 
 object Vectors {
     fun length(vector:Vector3f): Float {
@@ -23,5 +25,35 @@ object Vectors {
             (a.z * b.x) - (a.x * b.z),
             (a.x * b.y) - (a.y * b.x)
         )
+    }
+    fun dot_product(a:Vector3f,b: Vector3f): Float {
+        return ((length(a) * length(b) * cos(0f)))
+    }
+    fun translate(m:Matrix4f,v:Vector3f): Matrix4f {
+        var fbr = BufferUtils.createFloatBuffer(16)
+        fbr.put(m.get(0,0)).put(m.get(0,1)).put(m.get(0,2)).put(m.get(0,3))
+        fbr.put(m.get(1,0)).put(m.get(1,1)).put(m.get(1,2)).put(m.get(1,3))
+        fbr.put(m.get(2,0)).put(m.get(2,1)).put(m.get(2,2)).put(m.get(2,3))
+        // I know this is completely un-readable but its because newton says fuck you :)
+        fbr.put(dot_product(Vector3f(m.get(0,0),m.get(0,1),m.get(0,2)),v)).put(dot_product(Vector3f(m.get(1,0),m.get(1,1),m.get(1,2)),v)).put(dot_product(Vector3f(m.get(2,0),m.get(2,1),m.get(2,2)),v)).put(dot_product(Vector3f(m.get(3,0),m.get(3,1),m.get(3,2)),v))
+        return Matrix4f(fbr)
+    }
+    fun lookAt(eye: Vector3f, center: Vector3f, up: Vector3f): Matrix4f {
+        val f: Vector3f = normalize(subtract(center, eye))
+        var u: Vector3f = normalize(up)
+        val s: Vector3f = normalize(cross_product(f, u))
+        u = cross_product(s, f)
+        val result = BufferUtils.createFloatBuffer(16)
+        result.put(0f).put(0f).put(s.x)
+        result.put(1f).put(0f).put(s.y)
+        result.put(2f).put(0f).put(s.z)
+        result.put(0f).put(1f).put(u.x)
+        result.put(1f).put(1f).put(u.y)
+        result.put(2f).put(1f).put(u.z)
+        result.put(0f).put(2f).put(-f.x)
+        result.put(1f).put(2f).put(-f.y)
+        result.put(2f).put(2f).put(-f.z)
+        val resmatrix = Matrix4f(result)
+        return translate(resmatrix, Vector3f(-eye.x, -eye.y, -eye.z))
     }
 }
